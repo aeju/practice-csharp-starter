425118c37762eb54f2dd56d9a2245260b3566cf4
aeju <aejuchoe@naver.com>
2023-03-05 19:22:39 +0900

    Add: 0-1. Program.cs

---
+++ 0. Programmer Glasses/Program/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ParkingSystem
+{
+    class Program
+    {
+        static void Main()
+        {
+            string timeIn = DateTime.Now.ToString("yyyy-mm-dd:mm:ss");
+
+            int RegularParking = CheckRegularParking();
+
+            if (RegularParking == 1)
+            {
+                GateOpen();
+            }
+
+            else
+            {
+                PrintTicket(timeIn);
+
+                GateOpen();
+            }
+        }
+
+        static int CheckRegularParking()
+        {
+            int result;
+            bool IsRegistered = false;
+
+            if (IsRegistered == true)
+            {
+                result = 1;
+                return result;
+            }
+
+            else
+            {
+                result = 2;
+                return result;
+            }
+        }
+
+        static void GateOpen()
+        {
+            Console.WriteLine("차단기가 열립니다");
+        }
+
+        static void PrintTicket(string time)
+        {
+            Console.WriteLine("입차시간: {0}", time);
+        }
+    }
+}
