211c7a7a99037da350918ff4fb4cafb66436670b
aeju <aejuchoe@naver.com>
2023-03-05 21:02:28 +0900

    Add: 2-2. 변수 선언, 값의 배정

---
+++ 2.Basic/Variables.cs
@@ -0,0 +1,29 @@
+﻿/* 변수 선언 
+* 
+* 1) 자료형 변수명;
+* 2) 자료형 변수명 = 값;
+*/
+
+/*이스케이프 문자(이스케이프 시퀀스) : 특별한 기능 가진 문자
+ * ex. \n
+ * https://learn.microsoft.com/ko-kr/cpp/c-language/escape-sequences?view=msvc-170
+*/
+
+using System;
+
+namespace VariablesDemo
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            string name = "최애주";
+            char exmark = '!';
+            sbyte age;
+            age = 20;
+
+            Console.WriteLine("Hello " + name + exmark);
+            Console.Write("You are " + age + " years old" + exmark);
+        }
+    }
+}
